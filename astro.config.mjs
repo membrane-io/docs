c921bc1f653965fe7a94414ac6036aa593b3ce10
Justin Bennett <justin@membrane.io>
2024-04-28 12:44:59 -0400

    Drop landing page, start fleshing out docs, add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,48 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  redirects: {
+    "/": "/getting-started/intro-to-membrane/",
+  },
+  integrations: [
+    starlight({
+      title: "Membrane",
+      social: {
+        github: "https://github.com/withastro/starlight",
+        twitter: "https://twitter.com/membraneio",
+      },
+      sidebar: [
+        {
+          label: "Getting Started",
+          items: [
+            {
+              label: "Introduction to Membrane",
+              link: "/getting-started/intro-to-membrane/",
+            },
+          ],
+        },
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            {
+              label: "Example Guide",
+              link: "/guides/example/",
+            },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: {
+            directory: "reference",
+          },
+        },
+      ],
+    }),
+    tailwind(),
+  ],
 });
