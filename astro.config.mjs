1a0489352e91469dd9f1a2bf3cbef3e2e27a85dd
Peter Millspaugh <peterdgmillspaugh@gmail.com>
2024-06-13 06:33:21 -0700

    Enable ISR; server render public roadmap

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,13 @@ import cliHelpLang from "./cli-help.tmLanguage.json";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
+  output: "hybrid", // default to static, but allow SSR opt-in per page
+  adapter: vercel({
+    isr: {
+      // caches server rendered pages on first request and saves for 1 day
+      expiration: 60 * 60 * 24,
+    },
+  }),
   // This should be handled by a rewrite rule instead of being allowed to redirect
   redirects: {
     "/": "/getting-started/intro/",
@@ -195,5 +201,4 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
   ],
-  adapter: vercel(),
 });
