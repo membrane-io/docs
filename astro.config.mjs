c2d8e750fe4552018fc0593b9c7a1121b3cfbd62
Juan Campa <juancampa@gmail.com>
2025-01-02 19:52:31 -0500

    Tweak styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -46,7 +46,7 @@ export default defineConfig({
         shiki: {
           langs: [
             // Custom language for mctl help
-            JSON.parse(fs.readFileSync("./cli-help.tmLanguage.json", "utf-8")),
+            cliHelpLang,
           ],
         },
       },
