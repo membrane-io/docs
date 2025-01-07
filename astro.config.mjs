39281e9eaaaf7c482b5065acedcd54d5d49b5e9a
Juan Campa <juancampa@gmail.com>
2025-01-06 22:56:53 -0500

    Test algolia

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,15 @@ import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import starlightLinksValidator from "starlight-links-validator";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 import ecTwoSlash from "expressive-code-twoslash";
 import cliHelpLang from "./cli-help.tmLanguage.json";
+import starlightDocSearch from '@astrojs/starlight-docsearch';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://docs.membrane.io",
-  output: "hybrid", // default to static, but allow SSR opt-in per page
+  output: "static",
   adapter: vercel({
     isr: {
       // cache server rendered pages on first request and save for 1 hour
@@ -33,8 +34,8 @@ export default defineConfig({
       title: "Membrane",
       favicon: "/favicon.png",
       components: {
-        Head: "./src/components/Head.astro",
-        Search: "./src/components/Search.astro",
+        Head: "./src/components/Head.astro"
+        // Search: "./src/components/Search.astro",
       },
       editLink: {
         baseUrl: "https://github.com/membrane-io/docs/edit/main",
@@ -66,7 +67,14 @@ export default defineConfig({
         github: "https://github.com/membrane-io/docs",
         twitter: "https://twitter.com/membraneio",
       },
-      plugins: [starlightLinksValidator()],
+      plugins: [
+        starlightLinksValidator(),
+        starlightDocSearch({
+          appId: 'D16FPPVRA7',
+          apiKey: 'f01cc7df3a4e606df0dc9c2a61f894a9',
+          indexName: '7b446da1c86ad809763c402d865a1136',
+        }),
+      ],
       sidebar: [
         {
           label: "Getting Started",
